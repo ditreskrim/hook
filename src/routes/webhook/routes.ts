2ff8cf143aebdddd118166c46633f518505721a2
Dream <dontjustadream@outlook.com>
2023-05-22 19:44:59 +0700

    first message

--- src/routes/webhook/routes.ts
+++ src/routes/webhook/routes.ts
@@ -4,6 +4,15 @@ import {HandleRequest} from './controller';
 const routes: FastifyPluginAsync = async (fastify): Promise<void> => {
     fastify.post('/', {}, HandleRequest);
     fastify.get('/', {}, HandleRequest);
+    fastify.options('/', (_request, reply) => {
+        reply.header("Access-Control-Allow-Origin", "*");
+        reply.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+        reply.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        return reply.code(204);
+    });
 };
 
 export default routes;
