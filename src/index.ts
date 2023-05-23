30695b473f2f448aa7be9abd0a79783b7f5f6d97
Dream <dontjustadream@outlook.com>
2023-05-23 14:48:56 +0700

    webhook

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
+import fastify, {FastifyInstance, FastifyServerOptions} from 'fastify';
 import fastifyApp from './app';
+const ServerlessHttp = require('serverless-http');
 
 const default_options = {
-  ignoreTrailingSlash: true,
-  caseSensitive: false,
-  logger: {
-    transport: {
-      target: '@fastify/one-line-logger',
+    ignoreTrailingSlash: true,
+    caseSensitive: false,
+    logger: {
+        transport: {
+            target: '@fastify/one-line-logger',
+        },
     },
-  },
 };
 const Instance = async (options: FastifyServerOptions = default_options) => {
-  const instance: FastifyInstance = fastify(options);
-  void instance.register(fastifyApp);
-  void instance.listen();
-  return instance;
+    const instance: FastifyInstance = fastify(options);
+    void instance.register(fastifyApp);
+    void instance.listen();
+    return instance;
 };
 export const InstanceApp = Instance();
 export default Instance;
-export { Instance, default_options };
+export {Instance, default_options};
+module.exports.handler = ServerlessHttp(InstanceApp)
